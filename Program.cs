d4f4ee49d3d2a147286f34d029b2f9ff8c0008a1
Alexandre Mutel <alexandre_mutel@live.com>
2024-01-13 07:32:09 +0100

    Initial version

---
+++ Program.cs
@@ -0,0 +1,256 @@
+// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license.
+// See license.txt file in the project root for full license information.
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Fast1BRC;
+
+/// <summary>
+/// C# version for The One Billion Row Challenge
+/// https://github.com/gunnarmorling/1brc
+/// Author: xoofx
+/// </summary>
+internal static unsafe class Program
+{
+    private const int ReadBufferSize = 512 << 10; // 512 KB
+        
+    static void Main(string[] args)
+    {
+        var filePath = args.Length > 0 ? args[0] : Path.Combine(Environment.CurrentDirectory, "measurements-1_000_000-sample.txt");
+        if (!File.Exists(filePath)) throw new FileNotFoundException(filePath);
+
+        var clock = Stopwatch.StartNew();
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine(Run(filePath));
+        clock.Stop();
+
+        Console.WriteLine($"Elapsed in {clock.Elapsed.TotalMilliseconds} ms");
+    }
+
+
+    /// <summary>
+    /// Main entry point for the program
+    /// </summary>
+    /// <param name="filePath">File to process</param>
+    /// <returns>Formatted results</returns>
+    private static string Run(string filePath)
+    {
+        using var fileHandle = File.OpenHandle(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, FileOptions.SequentialScan);
+        var fileLength = RandomAccess.GetLength(fileHandle);
+
+        // --------------------------------------------------------------------
+        // Split the file by chunks and process them in parallel
+        // --------------------------------------------------------------------
+        Span<byte> localBuffer = stackalloc byte[256];
+        var taskCount = Math.Min((int)(fileLength / (ReadBufferSize * 4) + 1), Environment.ProcessorCount);
+        var tasks = new List<Task<Dictionary<ulong, EntryItem>>>(taskCount);
+        var chunkSize = fileLength / taskCount;
+        long startOffset = 0;
+        for (int i = 0; i < taskCount; i++)
+        {
+            long endOffset;
+            if (i + 1 < taskCount)
+            {
+                endOffset = (i + 1) * chunkSize;
+
+                // We always align to the end of the line
+                RandomAccess.Read(fileHandle, localBuffer, endOffset);
+                var indexOfEndOfLine = localBuffer.IndexOf((byte)'\n');
+                Debug.Assert(indexOfEndOfLine >= 0);
+                endOffset += indexOfEndOfLine + 1;
+            }
+            else
+            {
+                endOffset = fileLength;
+            }
+
+            long localStartOffset = startOffset;
+            tasks.Add(Task.Run(() => ProcessChunk(filePath, localStartOffset, endOffset)));
+            startOffset = endOffset;
+        }
+        Task.WaitAll(tasks.ToArray());
+
+        // --------------------------------------------------------------------
+        // Aggregate the results
+        // --------------------------------------------------------------------
+        var result = tasks.Select(x => x.Result).SelectMany(x => x.Values).Aggregate(
+            new Dictionary<string, EntryItem>(),
+            (acc, item) =>
+            {
+                string? name = item.Name!;
+                ref var existingValue = ref CollectionsMarshal.GetValueRefOrAddDefault(acc, name, out _);
+                existingValue.Name = name;
+                existingValue.AggregateFrom(ref item);
+                return acc;
+            }
+        );
+
+        // --------------------------------------------------------------------
+        // Format the results
+        // --------------------------------------------------------------------
+        var builder = new StringBuilder();
+        builder.Append('{');
+        bool isFirst = true;
+        foreach (var entryItem in result.Values.OrderBy(x => (string)x.Name!, StringComparer.Ordinal))
+        {
+            if (!isFirst) builder.Append(", ");
+            builder.Append($"{entryItem.Name}={(entryItem.MinTemp / 10.0):0.0}/{entryItem.SumTemp / (10.0 * entryItem.Count):0.0}/{(entryItem.MaxTemp / 10.0):0.0}");
+            isFirst = false;
+        }
+        builder.Append('}');
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Process a chunk of the file
+    /// </summary>
+    /// <param name="filePath">The file to process</param>
+    /// <param name="startOffset">The starting offset within the file. Always start at the beginning of a new line.</param>
+    /// <param name="endOffsetNotInclusive">The end offset - non inclusive.</param>
+    /// <returns></returns>
+    private static Dictionary<ulong, EntryItem> ProcessChunk(string filePath, long startOffset, long endOffsetNotInclusive)
+    {
+        // Reopen the file to improve concurrency, as it seems - at least on Windows - that it is more efficient to have multiple handles
+        using var localFileHandle = File.OpenHandle(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, FileOptions.SequentialScan);
+        var buffer = GC.AllocateUninitializedArray<byte>(ReadBufferSize);
+        ref var pBuffer = ref MemoryMarshal.GetArrayDataReference(buffer);
+        var entries = new Dictionary<ulong, EntryItem>(4000);
+
+        long fileOffset = startOffset;
+        nint bufferOffset = 0; // in case we have a line that is not fully read
+        while (fileOffset < endOffsetNotInclusive)
+        {
+            // Read the next buffer
+            var maxLengthToRead = Math.Min(ReadBufferSize - bufferOffset, endOffsetNotInclusive - fileOffset);
+            var span = MemoryMarshal.CreateSpan(ref Unsafe.Add(ref pBuffer, bufferOffset), (int)maxLengthToRead);
+            var bufferLength = RandomAccess.Read(localFileHandle, span, fileOffset);
+
+            if (bufferLength == 0) break;
+            fileOffset += bufferLength;
+            bufferLength += (int)bufferOffset;
+
+            // Process the buffer
+            var startLineIndex = ProcessBuffer(entries, ref pBuffer, bufferLength);
+
+            if (startLineIndex >= 0)
+            {
+                // Copy the remaining bytes to the beginning of the buffer (A line that was not fully read)
+                var remainingLength = bufferLength - (int)startLineIndex;
+                Unsafe.CopyBlockUnaligned(ref pBuffer, ref Unsafe.Add(ref pBuffer, startLineIndex), (uint)remainingLength);
+                bufferOffset = remainingLength;
+            }
+            else
+            {
+                bufferOffset = 0;
+            }
+        }
+
+        return entries;
+    }
+
+    /// <summary>
+    /// Process a buffer
+    /// </summary>
+    /// <param name="entries">The dictionary to add entries.</param>
+    /// <param name="pBuffer">The start of the buffer.</param>
+    /// <param name="bufferLength">The length of the buffer</param>
+    /// <returns>An index to the remaining buffer that hasn't been processed because the line was not complete; otherwise -1</returns>
+    private static nint ProcessBuffer(Dictionary<ulong, EntryItem> entries, ref byte pBuffer, nint bufferLength)
+    {
+        nint index = 0;
+        while (index < bufferLength)
+        {
+            nint startLineIndex = index;
+
+            // --------------------------------------------------------------------
+            // Process the name
+            // We calculate the FNV-1A hash as we go
+            // With a 64bit hash, we should avoid any collisions for the entries that we have
+            // --------------------------------------------------------------------
+            nint commaIndex;
+            ulong hash = 0xcbf29ce484222325;
+            while(index < bufferLength)
+            {
+                var c = Unsafe.Add(ref pBuffer, index);
+                if (c == (byte)';')
+                {
+                    commaIndex = index++;
+                    goto readTemp;
+                }
+                hash = (hash ^ c) * 0x100000001b3;
+                index++;
+            }
+            return startLineIndex;
+
+            readTemp:
+
+            // --------------------------------------------------------------------
+            // Process the temperature
+            // --------------------------------------------------------------------
+            int sign = 1;
+            int temp = 0;
+            while (index < bufferLength)
+            {
+                var c = Unsafe.Add(ref pBuffer, index++);
+                if (c == (byte) '-')
+                {
+                    sign = -1;
+                }
+                else if (char.IsAsciiDigit((char)c))
+                {
+                    temp = temp * 10 + (c - '0');
+                }
+                else if (c == (byte)'\n')
+                {
+                    temp *= sign;
+                    goto proceedEntry;
+                }
+            }
+
+            return startLineIndex;
+
+            proceedEntry:
+
+            // --------------------------------------------------------------------
+            // Add the entry
+            // --------------------------------------------------------------------
+            ref var entry = ref CollectionsMarshal.GetValueRefOrAddDefault(entries, hash, out var exists);
+            if (!exists)
+            {
+                var name = Encoding.UTF8.GetString(MemoryMarshal.CreateReadOnlySpan(ref Unsafe.Add(ref pBuffer, startLineIndex), (int)(commaIndex - startLineIndex)));
+                entry.Name = name;
+                entry.MinTemp = int.MaxValue;
+            }
+            entry.Count++;
+            entry.SumTemp += temp;
+            entry.MinTemp = Math.Min(entry.MinTemp, temp);
+            entry.MaxTemp = Math.Max(entry.MaxTemp, temp);
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Structure to hold the entry data per city
+    /// </summary>
+    private struct EntryItem
+    {
+        public string? Name;
+        public int Count;
+        public long SumTemp;
+        public int MinTemp;
+        public int MaxTemp;
+
+        public void AggregateFrom(ref EntryItem item)
+        {
+            SumTemp += item.SumTemp;
+            Count += item.Count;
+            MinTemp = Math.Min(item.MinTemp, MinTemp);
+            MaxTemp = Math.Max(item.MaxTemp, MaxTemp);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# 1️⃣🐝🏎️ The One Billion Row Challenge - .NET Edition
+
+> The One Billion Row Challenge (1BRC [Original Java Challenge](https://github.com/gunnarmorling/1brc)) is a fun exploration of how far modern .NET can be pushed for aggregating one billion rows from a text file.
+> Grab all your (virtual) threads, reach out to SIMD, optimize your GC, or pull any other trick, and create the fastest implementation for solving this task!
+
+Aggregated results for C#/F# at https://github.com/praeclarum/1brc
+
+## Build
+
+You need to have [.NET 8 SDK installed](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
+
+```
+dotnet publish -c Release -r win-x64
+.\bin\Release\net8.0\win-x64\publish\Fast1BRC measurements.txt
+```
+
+To fully test, you need to generate `measurements.txt`, easier on Ubuntu/MacOS:
+
+- Install OpenJDK 21 https://jdk.java.net/21/
+- Install Maven 3.9+ https://maven.apache.org/download.cgi
+- Clone `https://github.com/gunnarmorling/1brc` and go to its folder
+- Run `mvn package` 
+- Run `./create_measurements.sh 1000000000`
+- Copy `measurements.txt` to a place where you can use it with Fast1BRC
+
+## Results
+
+`1148.0469 ms` on my machine (AMD Ryzen 7950X at 60W)
+
+
+## License
+
+This software is released under the [BSD-2-Clause license](https://opensource.org/licenses/BSD-2-Clause). 
+
+## Author
+
+Alexandre Mutel aka [xoofx](https://xoofx.com).
